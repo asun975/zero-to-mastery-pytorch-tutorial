608fa7e3137a4b2a1ef4d57482bf88a93c6a6b8e
asun975 <sunas@sheridancollege.ca>
2024-06-01 19:43:56 -0400

    add file for computer vision models

---
+++ models/computervision_utils.py
@@ -0,0 +1,92 @@
+from torch import nn
+from torch import Tensor
+
+class FashionMNISTModelV0(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        """
+        Time to build a baseline model by subclassing nn.Module.
+
+        A baseline model is one of the simplest models you can imagine.
+
+        Our baseline will consist of two nn.Linear() layers.
+
+        We've done this in a previous section but there's going to one slight difference.
+
+        Because we're working with image data, we're going to use a different layer to start things off.
+
+        And that's the nn.Flatten() layer.
+
+        nn.Flatten() compresses the dimensions of a tensor into a single vector.
+        """
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(), # neural networks like their inputs in vector form
+            nn.Linear(in_features=input_shape, out_features=hidden_units), # in_features = number of features in a data sample (784 pixels)
+            nn.Linear(in_features=hidden_units, out_features=output_shape)
+        )
+    
+    def forward(self, x):
+        return self.layer_stack(x)
+
+class FashionMNISTModelV1(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        """Create a model with non-linear and linear layers."""
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(), # flatten inputs into single vector
+            nn.Linear(in_features=input_shape, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_shape),
+            nn.ReLU()
+        )
+    
+    def forward(self, x: Tensor):
+        return self.layer_stack(x)
+
+
+class FashionMNISTModelV2(nn.Module):
+    """
+    Model architecture copying TinyVGG from: 
+    https://poloclub.github.io/cnn-explainer/
+    """
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our inputs data.
+            nn.Linear(in_features=hidden_units*7*7, 
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: Tensor):
+        x = self.block_1(x)
+        # print(x.shape)
+        x = self.block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
