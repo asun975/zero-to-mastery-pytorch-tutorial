a0ca6a510a098392174a979814f107e548f67cc7
asun975 <sunas@sheridancollege.ca>
2024-06-01 18:48:33 -0400

    flake8 style enforcement

--- models/linear_regression_utils.py
+++ models/linear_regression_utils.py
@@ -1,32 +1,45 @@
 import torch
 from torch import nn
 
+
 class LinearRegressionModel(nn.Module):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.weights = nn.Parameter(
-            torch.randn(1, dtype=torch.float), 
-            requires_grad=True  # <- can we update this value with gradient descent?)
-        ) 
+            torch.randn(1, dtype=torch.float),
+            requires_grad=True
+        )
 
         self.bias = nn.Parameter(
-            torch.randn(1, dtype=torch.float), 
-            requires_grad=True  # <- can we update this value with gradient descent?))
-        ) 
+            torch.randn(1, dtype=torch.float),
+            requires_grad=True
+        )
 
     # Forward defines the computation in the model
     # "x" is the input data (e.g. training/testing features)
-    def forward(self, x: torch.Tensor) -> torch.Tensor: 
-        return self.weights * x + self.bias # (y = m*x + b)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.weights * x + self.bias  # (y = m*x + b)
 
-# Linear Regression model using nn.Linear() for creating model parameters
+
 class LinearRegressionModelV2(nn.Module):
+    """
+    Linear Regression model using nn.Linear() for
+    creating model parameters
+    """
     def __init__(self):
         super().__init__()
         # Use nn.Linear() for creating the model parameters
-        self.linear_layer = nn.Linear(in_features=1, 
-                                      out_features=1)
-    
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
     # Define the forward computation (input data x flows through nn.Linear())
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear_layer(x)
+
+
+torch.manual_seed(42)
+# Create an instance of the model (this is a subclass
+# of nn.Module that contains nn.Parameter(s))
+model_0 = LinearRegressionModel()
+
+# Check the nn.Parameter(s) within the nn.Module subclass we created
+print(list(model_0.parameters()))
