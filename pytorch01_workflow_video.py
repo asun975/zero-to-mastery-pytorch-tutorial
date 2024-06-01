0d1d6bb96e32aac8b35b661b956c47e26c5df043
asun975 <sunas@sheridancollege.ca>
2024-06-01 18:56:39 -0400

    add import for linear regression models

--- pytorch01_workflow_video.py
+++ pytorch01_workflow_video.py
@@ -4,6 +4,8 @@ import torch
 from torch import nn # building blocks for neural networks
 import matplotlib.pyplot as plt
 
+from models.linear_regression_utils import LinearRegressionModel, LinearRegressionModelV2
+
 # %%
 what_were_covering = {1: "data (prepare and load)",
     2: "build model",
@@ -74,7 +76,7 @@ plot_predictions()
 
 # 2. Build model
 # %% Create a Linear Regression model class
-class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
+"""class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
     def __init__(self):
         super().__init__() 
         self.weights = nn.Parameter(torch.randn(1, # <- start with random weights (this will get adjusted as the model learns)
@@ -87,7 +89,7 @@ class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a n
 
     # Forward defines the computation in the model
     def forward(self, x: torch.Tensor) -> torch.Tensor: # <- "x" is the input data (e.g. training/testing features)
-        return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)
+        return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)"""
 
 # %%  Check contents of pytorch model
 # Set manual seed since nn.Parameter are randomly initialzied
