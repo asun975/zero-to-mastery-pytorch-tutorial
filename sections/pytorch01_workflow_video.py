ef51896a8c2c907ef65478eac5a39a8703753d04
asun975 <sunas@sheridancollege.ca>
2024-06-02 19:25:15 -0400

    rename 'jupyter' folder to 'sections'

--- sections/pytorch01_workflow_video.py
+++ sections/pytorch01_workflow_video.py
@@ -6,14 +6,6 @@ import matplotlib.pyplot as plt
 
 from models.model_cls import LinearRegressionModel
 
-"""
-what_were_covering = {1: "data (prepare and load)",
-    2: "build model",
-    3: "fitting the model to data (training)",
-    4: "making predictions and evaluating a model (inference)",
-    5: "saving and loading a model",
-    6: "putting it all together"
-}"""
 
 # Check pytorch version
 torch.__version__
