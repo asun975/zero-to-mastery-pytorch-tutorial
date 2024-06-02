2ed7501c3613f615a9c475dde497ea5dccd4c4b7
asun975 <sunas@sheridancollege.ca>
2024-06-02 12:48:49 -0400

    modify creating data section moved to prepare_load_data.py

--- pytorch02_NN_Classification_multi-class_model.py
+++ pytorch02_NN_Classification_multi-class_model.py
@@ -15,27 +15,26 @@ Specifically, let's do the following:
 # %%
 # Import dependencies
 import torch
+from torch import nn
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
-#%%
+
+from prepare_load_data import sklearn_blobs_data
+
+
 # Set the hyperparameters for data creation
 NUM_CLASSES = 4
 NUM_FEATURES = 2
 RANDOM_SEED = 42
-#%%
-# 1. Create multi-class data
-X_blob, y_blob = make_blobs(n_samples=1000,
-    n_features=NUM_FEATURES, # X features
-    centers=NUM_CLASSES, # y labels 
-    cluster_std=1.5, # give the clusters a little shake up (try changing this to 1.0, the default)
-    random_state=RANDOM_SEED
-)
 
-# 2. Turn data into tensors
-X_blob = torch.from_numpy(X_blob).type(torch.float)
-y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
-print(X_blob[:5], y_blob[:5])
+# 1. Create multi-class data
+X_blob, y_blob = sklearn_blobs_data(
+   n_samples=1000, 
+   n_classes=NUM_CLASSES, 
+   n_features=NUM_FEATURES, 
+   random_seed=RANDOM_SEED
+   )
 
 # 3. Split into train and test sets
 X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
